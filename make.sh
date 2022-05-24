1d6073008fd4f8e6c5b7a455c2e9961859c6355a
Cas Brugman <casbrugman@hotmail.nl>
2022-05-24 18:06:31 +0200

    Nederlands gemaakt

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo building project-agv..
+echo project-agv compileren..
 read -p "op welke poort zit je arduino aangesloten? (COM2, COM3, ..):" poort
 poort=${poort:-COM3} 
 echo poort ${poort} word er gebruikt..
 make PORT=${poort}
-echo done, waiting for keypress..
+echo klaar, druk op een toets om dit venster te sluiten..
 read -rsn1
