64eacd64fb8c878d0b00e648e4252846473c282e
Cas Brugman <casbrugman@hotmail.nl>
2022-05-24 13:20:59 +0200

    Script vraagt welke poort je wilt gebruiken

--- make.sh
+++ make.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo building project-agv..
-make
+read -p "op welke poort zit je arduino aangesloten? (COM2, COM3, ..):" poort
+poort=${poort:-COM3} 
+echo poort ${poort} word er gebruikt..
+make PORT=${poort}
 echo done, waiting for keypress..
 read -rsn1
