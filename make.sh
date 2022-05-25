9afdbb44b4cd6b1794d0c2b6892dc0da0cb3ec82
Cas Brugman <casbrugman@hotmail.nl>
2022-05-25 16:56:51 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo project-agv compileren..
-read -p "op welke poort zit je arduino aangesloten? (COM2, COM3, ..):" poort
+read -p "op welke poort zit je arduino aangesloten? bijvoorbeeld: COM1, COM2.. (niks voor COM3):" poort
 poort=${poort:-COM3} 
 echo poort ${poort} word er gebruikt..
 make PORT=${poort}
