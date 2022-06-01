eeba3ae51dea1d4f8bf19be3a33bd679645ba384
Cas Brugman <casbrugman@hotmail.nl>
2022-06-01 10:44:53 +0200

    main aangepast

--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 #include "lib/pinio.h"
 #include "lib/serialio.h"
 #include "lib/tof/tof.h"
+#include "lib/adc.h"
+#include "lib/ldr/ldr.h"
 
 void init()
 {
     init_motoren();
     serialio_init();
-    tof_init();
+    adc_init();
+    init_ldr();
+    init_motoren();
+    // tof_init();
 }
 
 void start()
 {
     // pinio_test();
     // serialio_test();
-    tof_test();
+    // tof_test();
+    // adc_test();
+    // ldr_volgen();
+    RijdenX_as(0);
 }
 
 int main()
