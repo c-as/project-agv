d687025823f250c0d7b2246fa10a0bc6415a691b
Cas Brugman <casbrugman@hotmail.nl>
2022-06-01 10:56:08 +0200

    Gebruik adc.h

--- lib/ldr/ldr.c
+++ lib/ldr/ldr.c
@@ -10,101 +10,16 @@
 #define MIN_VERSCHIL 30
 #define MIN_LICHT 80
 
-// ADC Initialisatie
-void init_ldr(void)
-{
-    ADMUX = (0 << REFS1) | (1 << REFS0);
-    ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-    ADCSRA |= (1 << ADEN);
-}
-
-//int map(int x, int in_min, int in_max, int out_min, int out_max)
-//{
-//    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-//}
-//
-//// Voor LDR boven (vanaf bovenkant)
-//uint16_t get_LightLevel_1()
-//{
-//    _delay_ms(10);
-//    ADCSRA |= (1 << ADSC);
-//
-//    while (ADCSRA & (1 << ADSC))
-//        ;
-//
-//    _delay_ms(10);
-//    return (ADC); // Return 10-bit result
-//}
-//
-//// Voor LDR beneden (vanaf bovenkant)
-//uint16_t get_LightLevel_2()
-//{
-//    _delay_ms(10);
-//    ADCSRA |= (1 << ADSC);
-//
-//    while (ADCSRA & (1 << ADSC))
-//        ;
-//
-//    _delay_ms(10);
-//    return (ADC); // Return 10-bit result
-//}
-//
-//void ldr_vergelijken()
-//{
-//    // LDR 1 < 2
-//    if ((map(get_LightLevel_1(), 0, 1023, 0, 3)) < (map(get_LightLevel_2(), 0, 1023, 0, 3)))
-//    {
-//        // Rechtsom draaien
-//
-//        while (1)
-//        {
-//            if ((map(get_LightLevel_1(), 0, 1023, 0, 3)) == (map(get_LightLevel_2(), 0, 1023, 0, 3)))
-//            {
-//                // Stop motoren
-//                // Vooruit rijden
-//
-//                break;
-//            }
-//        }
-//    }
-//    // LDR 1 > 2
-//    else if ((map(get_LightLevel_1(), 0, 1023, 0, 3)) > (map(get_LightLevel_2(), 0, 1023, 0, 3)))
-//    {
-//        // Linksom draaien
-//
-//        while (1)
-//        {
-//            if ((map(get_LightLevel_1(), 0, 1023, 0, 3)) == (map(get_LightLevel_2(), 0, 1023, 0, 3)))
-//            {
-//                // Stop motoren
-//                // Vooruit rijden
-//
-//                break;
-//            }
-//        }
-//    }
-//}
-
-int get_light(int LDR) // choose which ldr to read
-{
-    ADMUX = 0b01000000; // read from A0: LDR boven (1)
-    if (LDR)
-    {
-        ADMUX = 0b01000001; // read from A1: LDR beneden (2)
-    }
-    ADCSRA |= (1 << ADSC);                 // start conversion
-    loop_until_bit_is_clear(ADCSRA, ADSC); // wait for conversion to finish
-
-    return ADC;
-}
+#define PIN_LDR0 MEGA_PIN_A0_ANALOG
+#define PIN_LDR1 MEGA_PIN_A1_ANALOG
 
 void ldr_vergelijken()
 {
     // verschil bestaat
-    if (abs(get_light(0) - get_light(1)) > MIN_VERSCHIL)
+    if (abs(adc_convert(PIN_LDR0) - adc_convert(PIN_LDR1)) > MIN_VERSCHIL)
     {
         // LDR 1 < 2
-        if (get_light(0) < get_light(1))
+        if (adc_convert(PIN_LDR0) < adc_convert(PIN_LDR1))
         {
             // Rechtsom draaien
             printf("ldr: motors rechts\n");
@@ -112,7 +27,7 @@ void ldr_vergelijken()
             while (1)
             {
                 // LDR 1 == 2
-                if (abs(get_light(0) - get_light(1)) < MIN_VERSCHIL)
+                if (abs(adc_convert(PIN_LDR0) - adc_convert(PIN_LDR1)) < MIN_VERSCHIL)
                 {
                     // motoren stoppen
                     printf("ldr: motors uit\n");
@@ -130,7 +45,7 @@ void ldr_vergelijken()
             while (1)
             {
                 // LDR 1 == 2
-                if (abs(get_light(0) - get_light(1)) < MIN_VERSCHIL) /// waarden geven wss niet precies dezelfde waarde, dus ff nog oplossing
+                if (abs(adc_convert(PIN_LDR0) - adc_convert(PIN_LDR1)) < MIN_VERSCHIL) /// waarden geven wss niet precies dezelfde waarde, dus ff nog oplossing
                 {
                     // motoren stoppen
                     printf("ldr: motors uit\n");
@@ -139,7 +54,6 @@ void ldr_vergelijken()
             }
         }
     }
-
 }
 
 void ldr_volgen()
@@ -147,7 +61,7 @@ void ldr_volgen()
     while (1)
     {
         // LDR > 100
-        if (get_light(0) < MIN_LICHT || get_light(1) < MIN_LICHT)
+        if (adc_convert(PIN_LDR0) < MIN_LICHT || adc_convert(PIN_LDR1) < MIN_LICHT)
         {
             // ToF 1 en 2 > 20
             if (1)
@@ -165,8 +79,8 @@ void ldr_volgen()
                 printf("ldr: motors uit\n");
             }
         }
-        printf("get_light0: %i\n", adc_convert(MEGA_PIN_A0_ANALOG));
-        printf("get_light1: %i\n", adc_convert(MEGA_PIN_A1_ANALOG));
+        printf("get_light0: %i\n", adc_convert(PIN_LDR0));
+        printf("get_light1: %i\n", adc_convert(PIN_LDR1));
         _delay_ms(100);
     }
 }
