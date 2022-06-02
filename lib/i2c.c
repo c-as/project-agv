6bfcd7d217958917bad9e20b5b6d6bd982d805d8
Cas Brugman <casbrugman@hotmail.nl>
2022-06-03 00:37:09 +0200

    Beginnetje i2c code

---
+++ lib/i2c.c
@@ -0,0 +1,77 @@
+#include <compat/twi.h>
+#include <stdio.h>
+
+#define SCL_CLOCK 400000
+#define STATUS_START_TRANSMITTED 0x08
+#define STATUS_SLA_W_TRANSMITTED 0x20
+#define STATUS_SLA_W_TRANSMITTED_ACK 0x18
+#define STATUS_DATA_TRANSMITTED 0x30
+#define STATUS_DATA_TRANSMITTED_ACK 0x28
+
+typedef enum
+{
+    OPERATION_READ,
+    OPERATION_WRITE,
+} I2COperation;
+
+void i2c_init()
+{
+    TWSR = 0;
+    TWBR = ((F_CPU / SCL_CLOCK) - 16) / 2;
+}
+
+void i2c_wait_twint()
+{
+    while (!(TWCR & (1 << TWINT)))
+    {
+    }
+}
+
+void i2c_send_start_condition()
+{
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    i2c_wait_twint();
+    if ((TWSR & 0xF8) != STATUS_START_TRANSMITTED) // Check value of TWI Status Register. Mask prescaler bits. If status different from START go to ERROR
+    {
+        printf("i2c error: start not transmitted\n");
+        return;
+    }
+}
+
+void i2c_send_operation(uint8_t address, I2COperation operation)
+{
+    TWDR = address;
+    if (operation == OPERATION_WRITE)
+    {
+        TWCR = (1 << TWINT) | (1 << TWEN);
+    }
+    else if (operation == OPERATION_READ)
+    {
+        printf("i2c error: operation read not implemented\n");
+    }
+    i2c_wait_twint();
+    if ((TWSR & 0xF8) != STATUS_SLA_W_TRANSMITTED_ACK) // Check value of TWI Status Register. Mask prescaler bits. If status different from MT_SLA_ACK go to ERRO
+    {
+        printf("i2c error: start not transmitted\n");
+        return;
+    }
+}
+
+void i2c_send_data(uint8_t data)
+{
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+}
+
+int i2c_write_register(uint8_t address, uint8_t data)
+{
+    i2c_send_start_condition();
+    i2c_send_operation(address, OPERATION_WRITE);
+}
+
+void i2c_test()
+{
+    while (1)
+    {
+    }
+}
---
+++ lib/i2c.h
@@ -0,0 +1,7 @@
+#ifndef _I2C_H_
+#define _I2C_H_
+
+void i2c_init();
+void i2c_test();
+
+#endif
