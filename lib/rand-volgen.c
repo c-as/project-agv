53b9124fe035594835eb25ae910623c1a18440ee
Cas Brugman <casbrugman@hotmail.nl>
2022-06-03 09:30:10 +0200

    Hele simpele feedback loop

--- lib/rand-volgen.c
+++ lib/rand-volgen.c
@@ -2,111 +2,34 @@
  */
 
 #include <avr/io.h>
-#include "defines.h"
+#include "rand-volgen.h"
+#include "tof/tof.h"
+#include <math.h>
+#include "motoren.h"
 
-//void motor1(int beweeg) // pins moeten nog goed worden gedefined
-//{
-//    if (beweeg == 1)
-//    {
-//        PORT_LPWM |= (1 << PIN_LPWM);
-//    }
-//    else if (beweeg == 0)
-//    {
-//        PORT_LPWM &= ~(1 << PIN_LPWM);
-//        PORT_RPWM &= ~(1 << PIN_RPWM);
-//    }
-//    else if (beweeg == -1)
-//    {
-//        PORT_RPWM |= (1 << PIN_RPWM);
-//    }
-//}
-//
-//void motor2(int beweeg) // pins moeten nog goed worden gedefined
-//{
-//    if (beweeg == 1)
-//    {
-//        PORT_LPWM |= (1 << PIN_LPWM);
-//    }
-//    else if (beweeg == 0)
-//    {
-//        PORT_LPWM &= ~(1 << PIN_LPWM);
-//        PORT_RPWM &= ~(1 << PIN_RPWM);
-//    }
-//    else if (beweeg == -1)
-//    {
-//        PORT_RPWM |= (1 << PIN_RPWM);
-//    }
-//}
-//
-//void motor3(int beweeg) // pins moeten nog goed worden gedefined
-//{
-//    if (beweeg == 1)
-//    {
-//        PORT_LPWM |= (1 << PIN_LPWM);
-//    }
-//    else if (beweeg == 0)
-//    {
-//        PORT_LPWM &= ~(1 << PIN_LPWM);
-//        PORT_RPWM &= ~(1 << PIN_RPWM);
-//    }
-//    else if (beweeg == -1)
-//    {
-//        PORT_RPWM |= (1 << PIN_RPWM);
-//    }
-//}
-//
-//void motor4(int beweeg) // pins moeten nog goed worden gedefined
-//{
-//    if (beweeg == 1)
-//    {
-//        PORT_LPWM |= (1 << PIN_LPWM);
-//    }
-//    else if (beweeg == 0)
-//    {
-//        PORT_LPWM &= ~(1 << PIN_LPWM);
-//        PORT_RPWM &= ~(1 << PIN_RPWM);
-//    }
-//    else if (beweeg == -1)
-//    {
-//        PORT_RPWM |= (1 << PIN_RPWM);
-//    }
-//}
-
-void bijsturenRandAf()
-{
-    // Motor 1 vooruit
-    motor(1, 1);
-    // Motor 2 achteruit
-    motor(2, -1);
-    // Motor 3 vooruit
-    motor(3, 1);
-    // Motor 4 achteruit
-    motor(4, -1);
-}
-
-void bijsturenNaarRandToe()
-{
-    // Motor 1 achteruit
-    motor(1, -1);
-    // Motor 2 vooruit
-    motor(2, 1);
-    // Motor 3 achteruit
-    motor(3, -1);
-    // Motor 4 vooruit
-    motor(4, 1);
-}
+// afstanden in mm
+#define RAND_AFSTAND 50
+#define MIN_DIFF 5
 
 void RandVolgen()
 {
-    // ToF 3 < 20
-    if (1)
+    while (1)
     {
-        bijsturenRandAf(); // Moet er nog onder: ToF 3 == 20 ?
-    }
-
-    // ToF 3 > 20
-    else if (1)
-    {
-        bijsturenNaarRandToe(); // Moet er nog onder: ToF 3 == 20 ?
+        int meting = tof_measure();
+        if (abs(RAND_AFSTAND - meting) > MIN_DIFF)
+        {
+            if (RAND_AFSTAND > meting)
+            {
+                RijdenY_as(RIJRICHTING_VOORUIT);
+            }
+            else
+            {
+                RijdenY_as(RIJRICHTING_ACHTERUIT);
+            }
+        }
+        else
+        {
+            RijdenX_as(RIJRICHTING_VOORUIT);
+        }
     }
 }
--- lib/rand-volgen.h
+++ lib/rand-volgen.h
@@ -1,36 +1,6 @@
-/// Motoren
-// Motor 1
-#define PIN_LPWM    PE5
-#define PORT_LPWM   PORTE
-#define DDR_LPWM    DDRE
+#ifndef _RAND_VOLGEN_H_
+#define _RAND_VOLGEN_H_
 
-#define PIN_RPWM    PE4
-#define PORT_RPWM   PORTE
-#define DDR_RPWM    DDRE
+void RandVolgen();
 
-// Motor 2
-#define PIN_LPWM    PE5
-#define PORT_LPWM   PORTE
-#define DDR_LPWM    DDRE
-
-#define PIN_RPWM    PE4
-#define PORT_RPWM   PORTE
-#define DDR_RPWM    DDRE
-
-// Motor 3
-#define PIN_LPWM    PE5
-#define PORT_LPWM   PORTE
-#define DDR_LPWM    DDRE
-
-#define PIN_RPWM    PE4
-#define PORT_RPWM   PORTE
-#define DDR_RPWM    DDRE
-
-// Motor 4
-#define PIN_LPWM    PE5
-#define PORT_LPWM   PORTE
-#define DDR_LPWM    DDRE
-
-#define PIN_RPWM    PE4
-#define PORT_RPWM   PORTE
-#define DDR_RPWM    DDRE
+#endif
