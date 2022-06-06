4f6770e10e62a988299021cc62a09589ce45b8ce
Cas Brugman <casbrugman@hotmail.nl>
2022-06-06 22:31:37 +0200

    Ook header

--- lib/pwm.h
+++ lib/pwm.h
@@ -13,7 +13,6 @@ typedef enum
 void pwm_init();
 void pwm_group_set_duty(PwmGroup group, uint8_t duty);
 void pwm_pin_set_group(DigitalPin pin, uint8_t i, PwmGroup group);
-void pwm_pin_set(DigitalPin pin, uint8_t i, PwmGroup group, uint8_t duty);
 void pwm_test();
 
 #endif
