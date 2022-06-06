7a8b883b93f458a41eef5c92f81beb31d50a131d
Cas Brugman <casbrugman@hotmail.nl>
2022-06-06 14:54:26 +0200

    ; vergeten

--- lib/Stil-staan.c
+++ lib/Stil-staan.c
@@ -5,8 +5,8 @@
 
 void stil_staan()
 {
-    motor(1, MOTORRICHTING_STOP)
-    motor(2, MOTORRICHTING_STOP)
-    motor(3, MOTORRICHTING_STOP)
-    motor(4, MOTORRICHTING_STOP)
+    motor(1, MOTORRICHTING_STOP);
+    motor(2, MOTORRICHTING_STOP);
+    motor(3, MOTORRICHTING_STOP);
+    motor(4, MOTORRICHTING_STOP);
 }
