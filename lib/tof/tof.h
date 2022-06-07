b9918bed2c91a684644304d8066c250720e4dcc9
Cas Brugman <casbrugman@hotmail.nl>
2022-06-07 13:04:29 +0200

    Tof pins defined

--- lib/tof/tof.h
+++ lib/tof/tof.h
@@ -3,6 +3,11 @@
 
 #include "../pinio.h"
 
+#define TOF_1_PIN MEGA_PIN_D44_DIGITAL
+#define TOF_2_PIN MEGA_PIN_D45_DIGITAL
+#define TOF_3_PIN MEGA_PIN_D46_DIGITAL
+#define TOF_4_PIN MEGA_PIN_D47_DIGITAL
+
 void tof_init();
 uint16_t tof_measure(DigitalPin tof_vin_pin);
 void tof_test();
