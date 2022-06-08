95d9c22be6b27d170d072f2fec2377b3fdde52c2
Cas Brugman <casbrugman@hotmail.nl>
2022-06-08 10:02:15 +0200

    Duideluk

--- lib/tof/tof.h
+++ lib/tof/tof.h
@@ -3,10 +3,10 @@
 
 #include "../pinio.h"
 
-#define TOF_1_PIN MEGA_PIN_D44_DIGITAL
-#define TOF_2_PIN MEGA_PIN_D45_DIGITAL
-#define TOF_3_PIN MEGA_PIN_D46_DIGITAL
-#define TOF_4_PIN MEGA_PIN_D47_DIGITAL
+#define TOF_1_PIN_X MEGA_PIN_D44_DIGITAL
+#define TOF_2_PIN_X MEGA_PIN_D45_DIGITAL
+#define TOF_3_PIN_X MEGA_PIN_D46_DIGITAL
+#define TOF_4_PIN_X MEGA_PIN_D47_DIGITAL
 
 void tof_init();
 uint16_t tof_measure(DigitalPin tof_vin_pin);
