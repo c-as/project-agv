37a418568ae44b15d0279ceff0517b1b114cda1c
Cas Brugman <casbrugman@hotmail.nl>
2022-06-17 18:52:23 +0200

    Voor nu even omdat compileert niet maar ik heb beter idee

--- lib/ir_sensor.c
+++ lib/ir_sensor.c
@@ -1,34 +1,34 @@
-#include <avr/io.h>
-#include "motoren.h"
-#define IRsensor3 PC6
-#define IRsensor4 PC7
+// #include <avr/io.h>
+// #include "motoren.h"
+// #define IRsensor3 PC6
+// #define IRsensor4 PC7
 
-void initTimer0 (void)
-{
-    TCCR0A = 0;
-    TCCR0B = TCCR0B | (1<<CS01) | (1<<CS00);
-    TIMSK0 = TIMSK0 | (1<< TOIE0);
-}
+// void initTimer0 (void)
+// {
+//     TCCR0A = 0;
+//     TCCR0B = TCCR0B | (1<<CS01) | (1<<CS00);
+//     TIMSK0 = TIMSK0 | (1<< TOIE0);
+// }
 
-void IRsensoren ()
-{
-    DDRC &= ~(1<<IRsensor3);
-    DDRC &= ~(1<<IRsensor4);
-}
+// void IRsensoren ()
+// {
+//     DDRC &= ~(1<<IRsensor3);
+//     DDRC &= ~(1<<IRsensor4);
+// }
 
-ISR (TIMER0_OVF_vect)
-{
-    int IRsensor3_x = !(PINC &(1 << IRsensor3));
-    int IRsensor4_x = !(PINC &(1 << IRsensor4));
+// ISR (TIMER0_OVF_vect)
+// {
+//     int IRsensor3_x = !(PINC &(1 << IRsensor3));
+//     int IRsensor4_x = !(PINC &(1 << IRsensor4));
 
-    if (IRsensor3_x)
-    {
-        rijden_stop();
-    }
+//     if (IRsensor3_x)
+//     {
+//         rijden_stop();
+//     }
 
-    else if (IRsensor4_x)
-    {
-        rijden_stop();
-    }
+//     else if (IRsensor4_x)
+//     {
+//         rijden_stop();
+//     }
 
-}
+// }
