09ac3a45824652e0d141c544f4a420f73e5fb15f
Cas Brugman <casbrugman@hotmail.nl>
2022-06-19 22:04:27 +0200

    Wacht functies (maar deze mogen wel want deze maken gebruik van timers)

---
+++ lib/time.c
@@ -0,0 +1,27 @@
+#include <inttypes.h>
+#include "tof/vl53l0x-non-arduino/util/millis.h"
+#include "pinio.h"
+#include <avr/io.h>
+
+void wacht_millis(uint32_t wait)
+{
+    uint32_t start = millis();
+
+    while (millis() - start <= wait)
+    {
+        ;
+    }
+}
+
+void time_test()
+{
+    pin_set_mode(MEGA_PIN_D23_DIGITAL, PINMODE_DIGITAL_OUTPUT);
+
+    while (1)
+    {
+        pin_set_output(MEGA_PIN_D23_DIGITAL, 1);
+        wacht_millis(1000);
+        pin_set_output(MEGA_PIN_D23_DIGITAL, 0);
+        wacht_millis(1000);
+    }
+}
---
+++ lib/time.h
@@ -0,0 +1,7 @@
+#ifndef _TIME_H_
+#define _TIME_H_
+
+void wacht_millis(uint32_t wait);
+void time_test();
+
+#endif
