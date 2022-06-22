a763b70c6a8161953de24c99494bb6806969951c
Cas Brugman <casbrugman@hotmail.nl>
2022-06-22 16:11:05 +0200

    Functies toegevoegd aan header

--- lib/navigatie.h
+++ lib/navigatie.h
@@ -1,6 +1,14 @@
 #ifndef _NAVIGATIE_H
 #define _NAVIGATIE_H
 
+DigitalPin rijrichting_tof(RijRichting richting);
+int agv_muur_afstand(RijRichting richting, int afstand_mm);
+int agv_muren_midden(RijRichting ene_kant);
+void agv_zet_vooruit();
+void agv_zet_recht(RijRichting probe_muur);
+int agv_volg_rand_target(RijRichting volg_muur, RijRichting target_muur, uint16_t target_afstand_mm);
+void baan_wisselen();
+void agv_start_positie();
 void agv_start_navigatie();
 
 #endif
